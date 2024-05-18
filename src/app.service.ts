3dd7dddc4cf8983782b79bbb81d367db5a2ed673
vgvishesh <vgvishesh2008@gmail.com>
2024-05-18 11:15:55 +0530

    more leet code problem solutions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3764,6 +3764,25 @@ export class DSAService {
     return time;
   };
 
+  countVowels(word: string): number {
+    let p = 0;
+    let ts: number[] = [];
+    let n = word.length;
+    let total = 0;
+    for (let i = 0; i < n; i++) {
+      let numStrings = (n - i) + (p - i);
+      ts.push(numStrings);
+      p = numStrings;
+    }
+
+    for (let i = 0; i < n; i++) {
+      if ('aeiou'.includes(word[i])) {
+        total += ts[i];
+      }
+    }
+
+    return total;
+  };
 
 };
 
